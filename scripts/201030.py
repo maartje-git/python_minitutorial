ea8e9cfcdd626ecf9e32141bcedc6d3d042b6be3
Maartje <78266071+maartje-git@users.noreply.github.com>
2021-02-05 14:31:12 +0100

    Re-organized the python tutorial files.
    Divided into scripts, raw_data and output

---
+++ scripts/201030.py
@@ -0,0 +1,16 @@
+import pandas as pd, numpy as np
+#scientific python module scipy.org
+from scipy import stats
+
+data = pd.read_csv(
+    "C:/Users/dnalab/Desktop/mini tutorial/201030/MgCa_data_field.csv")
+
+#edit things in code, so you don't have to change everything seperately
+data.rename(columns={"Del-CO32-": "delCO3", "T": "temperature"}, inplace=True)
+
+#do linear regression
+#.values na luisteren
+l = ~np.isnan(data.temperature) & ~np.isnan(data.delCO3)
+slope, intercept, r, p, se = stats.linregress(data[l].temperature.values,
+                                            data[l].delCO3.values)
+
---
+++ scripts/210129.py
@@ -0,0 +1,6 @@
+x = 5
+print(x + 9)
+
+name = "Maartje"
+animal = "cat"
+number = "8"
