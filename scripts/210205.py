05c7f0afba354c90756477712b95eb2301155ce7
Maartjes_Git <brouwer.mm@gmail.com>
2021-02-05 14:52:01 +0100

    changed 210205.py to work with the new directory organization

--- scripts/210205.py
+++ scripts/210205.py
@@ -3,8 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # import file
-file_to_use = "titration2"
-data = np.genfromtxt(file_to_use + '.txt', skip_header=2)   #skips first 2 lines
+file_to_use = "titration1"
+data = np.genfromtxt("../raw_data/" + file_to_use + ".txt", skip_header=2)   #skips first 2 lines
 
 volume = data[:,0]  #: is all rows, column 1
 emf = data[:,1]
@@ -22,11 +22,11 @@ emf_std = np.std(emf)
 
 # Make a plot
 fig, ax = plt.subplots(dpi=300)
-ax.scatter(volume,emf, s=35, c='xkcd:vomit') #S = size of points, c= color, 
+ax.scatter(volume,emf, s=35, c='xkcd:periwinkle') #S = size of points, c= color, 
 ax.set_xlabel('volume / mL')
 ax.set_ylabel('EMF / mV')
 ax.grid(alpha=0.3) #alpha is transparancy
 
 #Save figure to file
-plt.savefig("figures/" + file_to_use + ".png")
-plt.savefig("figures/" + file_to_use + ".pdf")
+plt.savefig("../output/210205_" + file_to_use + "_VOLUMEvsEMF.png")
+
