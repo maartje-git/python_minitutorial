88da4846b3e928aa1cb2a9ddd7f71fcf96d31cdc
Maartje <brouwer.mm@gmail.com>
2021-10-04 13:40:14 +0200

    python tutorial stuff sept 2021

---
+++ autumn 2021/210917.py
@@ -0,0 +1,193 @@
+# Import and plot .csv
+import pandas as pd
+import numpy as np
+
+ctd1 = pd.read_csv(
+    "data/ctd-bottles/ctd-bottles-2-1.csv", 
+    na_values=-999
+    )
+ ##-999 is often used instead of NaN... 
+ctd1.plot.scatter("salinity", "depth")
+
+#%% #!!!HOMEWORK
+# import the variant files... get the same plot
+
+# This is how it should look like
+ctd1 = pd.read_csv(
+    "data/ctd-bottles-variants/00-ctd-bottles-2-1.csv", 
+    na_values=-999,
+    )
+#%%
+# first 3 rows is general info, so skip in dataframe
+ctd1 = pd.read_csv(
+    "data/ctd-bottles-variants/01-ctd-bottles-2-1.csv", 
+    na_values = -999, 
+    skiprows = 3 #skip first 3 rows 
+    )
+
+#%%
+# Second row indicates measurement units
+ctd1 = pd.read_csv(
+    "data/ctd-bottles-variants/02-ctd-bottles-2-1.csv", 
+    na_values=-999, 
+    skiprows=[1], #skip second row 
+    encoding="unicode_escape" #to make csv understand ° symbol
+    ) 
+
+# OR 
+ctd1 = pd.read_csv(
+    "data/ctd-bottles-variants/02-ctd-bottles-2-1.csv", 
+    na_values=-999, 
+    header=[0,1], #assign 2 rows of header to keep the units
+    encoding="unicode_escape"
+    ) 
+
+#%%
+# first 3 rows with general info, 5th row indicates measurement units
+ctd1 = pd.read_csv(
+    "data/ctd-bottles-variants/03-ctd-bottles-2-1.csv", 
+    na_values=-999, 
+    skiprows=3, #skip first 3 rows
+    header=[0,1], #assign 2 rows of header to keep the units
+    encoding="unicode_escape"
+    )
+# OR
+ctd1 = pd.read_csv(
+    "data/ctd-bottles-variants/03-ctd-bottles-2-1.csv", 
+    na_values=-999, 
+    header=[3,4], #assign 2 rows of header to keep the units
+    # Header automatically skips the row above it
+    encoding="unicode_escape"
+    )
+
+#%%  
+# .txt file
+ctd1 = pd.read_table(
+    "data/ctd-bottles-variants/04-ctd-bottles-2-1.txt", 
+    na_values=-999, 
+    header=[3,4],
+    encoding="unicode_escape"
+    )
+
+# OR
+ctd1 = pd.read_csv(
+    "data/ctd-bottles-variants/04-ctd-bottles-2-1.txt", 
+    na_values=-999, 
+    header=[3,4],
+    encoding="unicode_escape",
+    delimiter="\t" #tab delimited txt, 
+    )
+
+#%%
+# .xlsx file
+ctd1 = pd.read_excel(
+    "data/ctd-bottles-variants/05-ctd-bottles-2-1.xlsx", 
+    na_values=-999, 
+    header=[3,4],
+    )
+  ## already knows the special symbols, so no encoding necesarry
+
+#%%
+# 2 different NaN values
+ctd1 = pd.read_excel(
+    "data/ctd-bottles-variants/06-ctd-bottles-2-1.xlsx", 
+    na_values=[-999,-777], 
+    header=[3,4]
+    )
+
+#%%
+# different column names
+ctd1 = pd.read_excel(
+    "data/ctd-bottles-variants/07-ctd-bottles-2-1.xlsx", 
+    na_values=[-999,-777], 
+    header=[3,4]
+    ).rename(columns={
+    'Practical salinity':'salinity',
+    'Depth':'depth'})
+
+#%%
+# csv with different column names and a footer
+ctd1 = pd.read_csv(
+    "data/ctd-bottles-variants/08-ctd-bottles-2-1.csv", 
+    na_values=[-999,-777], 
+    header=[3,4],
+    skipfooter=3,
+    encoding="unicode_escape",
+    ).rename(columns={
+    'Practical salinity':'salinity',
+    'Depth':'depth'})
+
+#%%
+ctd1.plot.scatter("salinity", "depth")
+
+ 
+#%% Import lat/lon/station info (.txt)
+
+stations = pd.read_table("data/stations.txt", encoding="unicode_escape")
+ ## encoding="unicode_escape" <== to work with symbols like °)
+
+## Parse station information
+#row = stations.iloc[1]  #position 0 is the first
+  ##.loc to access a row in a dataframe using index
+  ##.iloc to access a row using the position in the dataframe
+
+#x = row.Latitude        #results in a string
+#x = float(row.Latitude) #results in a numder (float)
+
+#Different lon/lat formats
+# 54°00'04"S
+# -53.5000
+# 50°30.00'S
+# https://regex101.com
+# (\d+)°(\d+\.\d+)'([NS])
+
+import re #to be able to use regular expressions
+
+# regular expression for decimal minutes format
+dmf1 = re.match("(\d+)°(\d+\.\d+)'([NS])", "50°30.00'S")
+lat = round(float(dmf1.group(1)) + float(dmf1.group(2)) / 60, 4)
+if dmf1.group(3) == "S":
+    lat *= -1 # *= indicates self * so here: lat * -1
+    
+##!!!HOMEWORK
+# make regular expression for longitude (E/W)
+dmf2 = re.match("(\d+)°(\d+\.\d+)'([WE])", "000°29.13'E")
+lon = round(float(dmf2.group(1)) + float(dmf2.group(2)) / 60, 4)
+if dmf2.group(3) == "W":
+    lon *= -1 # *= indicates self * so here: lon * -1
+
+# make regular expressions for the other format
+dmf3 = re.match("(\d+)°(\d+)'(\d+)\"([NS])", "54°00'04\"S")
+minutes = dmf3.group(2) + "." + dmf3.group(3)
+lat2 = round(float(dmf3.group(1)) + float(minutes) / 60,4)
+if dmf3.group(4) == "S":
+    lat2 *= -1 # *= indicates self * so here: lat * -1
+
+dmf4 = re.match("(\d+)°(\d+)'(\d+)\"([EW])", "000°00'01\"E")
+minutes = dmf4.group(2) + "." + dmf4.group(3)
+lon2 = round(float(dmf4.group(1)) + float(minutes) / 60, 4)
+if dmf4.group(4) == "W":
+    lon2 *= -1 # *= indicates self * so here: lat * -1
+
+
+#%%
+# change all Lon/Lat values in floats instead of strings
+stations["lat"] = np.nan
+stations["lon"] = np.nan
+  ##make new column in the dataframe, with nan as values
+for i, station in stations.iterrows():
+    ## i = index#, station = all info of that row
+    try:
+        stations.loc[i, "lat"] = float(stations.loc[i, "Latitude"])
+        stations.loc[i, "lon"] = float(stations.loc[i, "Longitude"])
+        ##try this, if it doesn't work to make a float, continue with except)
+    except:
+        pass
+          ##do nothing and move on
+          
+
+# stations.plot.scatter("Longitude", "Latitude")
+#   ##weird plot, because of the symbols in the data
+
+# stations.plot.scatter("lon", "lat")
+#   ##nice plot, but still missing many data points
---
+++ autumn 2021/prep/explore_glodap.py
@@ -0,0 +1,242 @@
+import pandas as pd, numpy as np
+from matplotlib import pyplot as plt
+import calkulate as calk
+
+# Initialise random number generator
+rng = np.random.default_rng(7)
+
+# Import GLODAP Atlantic dataset and tidy up
+glodap = pd.read_csv(
+    # "/home/matthew/data/glodap/GLODAPv2.2021_Atlantic_Ocean.csv",  # Ubuntu
+    "C:/Users/mphum/Documents/data/GLODAP/GLODAPv2.2021_Atlantic_Ocean.csv",  # Windows
+    na_values=-9999,
+)
+cols_G2 = {col: col.replace("G2", "") for col in glodap.columns}
+glodap.rename(columns=cols_G2, inplace=True)
+glodap.rename(columns={"tco2": "dic", "talk": "alkalinity"}, inplace=True)
+
+#%% Find lat/lon/depth min/max of all cruises
+lldmm = (
+    glodap[["cruise", "longitude", "latitude", "depth"]]
+    .groupby(by="cruise")
+    .agg([np.min, np.max, np.size])
+)
+
+# Cut to cruises which cross both equator AND prime meridian, and that go deep
+lon = lldmm.longitude
+lat = lldmm.latitude
+lldmm = lldmm[
+    (lon.amin < 0)
+    & (lon.amax > 0)
+    & (lat.amin < 0)
+    & (lat.amax > 0)
+    & (lldmm.depth.amax > 5000)
+]
+
+# Plot those cruises
+fig, ax = plt.subplots(dpi=300)
+for cruise in lldmm.index:
+    ax.scatter(
+        "longitude",
+        "latitude",
+        data=glodap[glodap.cruise == cruise],
+        s=10,
+        edgecolor="none",
+        label=cruise,
+    )
+ax.legend()
+ax.grid(alpha=0.3)
+
+#%% Plot just one cruise
+cruise = 346
+fig, ax = plt.subplots(dpi=300)
+ax.scatter("latitude", "depth", c="salinity", data=glodap[glodap.cruise == cruise], s=5)
+ax.invert_yaxis()
+
+#%% Extract data for the selected cruise and reorganise
+data = glodap[glodap.cruise == cruise].copy()
+data["station"] = data.station.astype(int)
+data["cast"] = data.cast.astype(int)
+
+# Renumber bottles for simplicity
+b = 0
+s = data.station.iloc[0]
+for i, row in data.iterrows():
+    if row.station == s:
+        b += 1
+        data.loc[i, "bottle"] = b
+    else:
+        b = 1
+        data.loc[i, "bottle"] = b
+        s += 1
+
+# Get stations, convert to degrees and minutes, add errors, write to text file
+stations = (
+    data[["station", "cast", "latitude", "longitude"]]
+    .groupby(["station", "cast"])
+    .mean()
+)
+with open("data/stations.txt", "w") as f:
+    f.write("Station no.\tCast\tLatitude\tLongitude\n")
+    for s, row in stations.iterrows():
+        lat_deg = np.floor(np.abs(row.latitude))
+        lat_dec_mins = (np.abs(row.latitude) - lat_deg) * 60
+        lat_dir = "N" if row.latitude > 0 else "S"
+        lat_mins = np.floor(lat_dec_mins)
+        lat_secs = (lat_dec_mins - lat_mins) * 60
+        lon_deg = np.floor(np.abs(row.longitude))
+        lon_dec_mins = (np.abs(row.longitude) - lon_deg) * 60
+        lon_dir = "E" if row.longitude > 0 else "W"
+        lon_mins = np.floor(lon_dec_mins)
+        lon_secs = (lon_dec_mins - lon_mins) * 60
+        s_random = rng.integers(3)
+        if s_random == 0:
+            f.write(
+                (
+                    "{station:.0f}\t{cast:.0f}\t"
+                    + "{lat_deg:02.0f}°{lat_mins:05.2f}'{lat_dir}\t"
+                    + "{lon_deg:03.0f}°{lon_mins:05.2f}'{lon_dir}\n"
+                ).format(
+                    station=s[0],
+                    cast=s[1],
+                    lat_deg=lat_deg,
+                    lat_mins=lat_dec_mins,
+                    lat_dir=lat_dir,
+                    lon_deg=lon_deg,
+                    lon_mins=lon_dec_mins,
+                    lon_dir=lon_dir,
+                )
+            )
+        elif s_random == 1:
+            f.write(
+                ("{station:.0f}\t{cast:.0f}\t" + "{lat:.4f}\t" + "{lon:.4f}\n").format(
+                    station=s[0], cast=s[1], lat=row.latitude, lon=row.longitude
+                )
+            )
+        else:
+            lat_dir = "S"  # deliberate error
+            f.write(
+                (
+                    "{station:.0f}\t{cast:.0f}\t"
+                    + "{lat_deg:02.0f}°{lat_mins:02.0f}'{lat_secs:02.0f}\"{lat_dir}\t"
+                    + "{lon_deg:03.0f}°{lon_mins:02.0f}'{lon_secs:02.0f}\"{lon_dir}\n"
+                ).format(
+                    station=s[0],
+                    cast=s[1],
+                    lat_deg=lat_deg,
+                    lat_mins=lat_mins,
+                    lat_secs=lat_secs,
+                    lat_dir=lat_dir,
+                    lon_deg=lon_deg,
+                    lon_mins=lon_mins,
+                    lon_secs=lon_secs,
+                    lon_dir=lon_dir,
+                )
+            )
+
+#%% Extract nutrient data
+data["density25"] = calk.density.seawater_1atm_MP81(
+    temperature=25, salinity=data.salinity
+)
+nuts_list = ["nitrate", "nitrite", "silicate", "phosphate"]
+for nut in nuts_list:
+    data["{}_vol".format(nut)] = data[nut] * data.density25
+data["nitrate_nitrite_vol"] = data.nitrate_vol + data.nitrite_vol
+nuts = data[
+    [
+        "station",
+        "cast",
+        "bottle",
+        "nitrate_nitrite_vol",
+        "nitrite_vol",
+        "silicate_vol",
+        "phosphate_vol",
+    ]
+].copy()
+
+# Construct new station-cast-bottle column
+nuts["sample_id"] = ""
+s = -999
+for i, row in nuts.iterrows():
+    if row.station == s:
+        nuts.loc[i, "sample_id"] = "{:.0f}".format(row.bottle)
+    else:
+        nuts.loc[i, "sample_id"] = "{:.0f}-{:.0f}-{:.0f}".format(
+            row.station, row.cast, row.bottle
+        )
+        s = row.station
+nuts = nuts[
+    [
+        "sample_id",
+        "nitrate_nitrite_vol",
+        "nitrite_vol",
+        "silicate_vol",
+        "phosphate_vol",
+    ]
+]
+nuts = nuts[
+    ~pd.isnull(nuts.nitrate_nitrite_vol)
+    | ~pd.isnull(nuts.nitrite_vol)
+    | ~pd.isnull(nuts.silicate_vol)
+    | ~pd.isnull(nuts.phosphate_vol)
+]
+
+# Replace zeros with less-thans
+nuts.loc[nuts.nitrite_vol == 0, "nitrite_vol"] = "<0.01"
+nuts.loc[nuts.nitrate_nitrite_vol == 0, "nitrate_nitrite_vol"] = "<0.01"
+nuts.loc[nuts.phosphate_vol == 0, "phosphate_vol"] = "<0.01"
+
+# Rename columns and save to file
+nuts.rename(
+    columns={
+        "sample_id": "Sample ID",
+        "nitrate_nitrite_vol": "NO3+NO2",
+        "nitrite_vol": "NO2",
+        "silicate_vol": "Si",
+        "phosphate_vol": "PO4",
+    },
+    inplace=True,
+)
+excel_file = "data/nutrients.xlsx"
+nuts.to_excel(
+    excel_file, sheet_name="Station {:.0f}".format(s), index=False, na_rep="n.a."
+)
+
+#%% Extract CTD data
+ctd = data[
+    [
+        "station",
+        "cast",
+        "year",
+        "month",
+        "day",
+        "hour",
+        "minute",
+        "bottomdepth",
+        "bottle",
+        "pressure",
+        "depth",
+        "temperature",
+        "salinity",
+        "oxygen",
+    ]
+].copy()
+ctd.rename(columns={"bottomdepth": "bottom_depth"}, inplace=True)
+
+# Add oxygen and salinity calibrations
+oxygen_slope = 1.03  # slope of oxygen calibration
+oxygen_offset = 4.7  # offset in oxygen calibration
+oxygen_std = 1.0  # 1-sigma uncertainty in discrete oxygen measurements
+oxygen_true = ctd.oxygen.copy()
+ctd["oxygen"] = (oxygen_true - oxygen_offset) / oxygen_slope
+salinity_slope = 0.991  # slope of salinity calibration
+salinity_offset = 0.28  # offset in salinity calibration
+salinity_std = 0.01  # 1-sigma uncertainty in discrete salinity measurements
+salinity_true = ctd.salinity.copy()
+ctd["salinity"] = (salinity_true - salinity_offset) / salinity_slope
+
+for s, srow in stations.iterrows():
+    S = (ctd.station == s[0]) & (ctd.cast == s[1])
+    ctd[S].drop(columns=["station", "cast"]).to_csv(
+        "data/ctd-bottles/ctd-bottles-{}-{}.csv".format(*s), na_rep=-999, index=False
+    )
